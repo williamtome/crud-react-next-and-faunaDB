97db190476e9e4072152762b96303a10bd7e0b81
William Tomé <william.tomenh@gmail.com>
2020-12-05 23:26:33 -0300

    Send data if post method.

--- pages/api/contacts.js
+++ pages/api/contacts.js
@@ -3,16 +3,22 @@ import { q, client } from '../../utils/db'
 const getAllContacts = () => {
   return client.query(
     q.Map(
-      q.Paginate(q.Match('allContacts'), {
-        size: 2
-      }),
+      q.Paginate(q.Match('allContacts'), { size: 2 }),
       q.Lambda(x => q.Get(x))
     )
   )
 }
 
+const createContacts = data => {
+  return client.query(q.Create(q.Collection('contacts'), { data }))
+}
+
 export default async function handler(req, res) {
-  const contacts = await getAllContacts()
-  
-  res.json(contacts)
+  if(req.method === 'POST'){
+    const contact = await createContacts(req.body)
+    res.send(contact)
+  } else {
+    const contacts = await getAllContacts()
+    res.json(contacts)
+  }
 }
